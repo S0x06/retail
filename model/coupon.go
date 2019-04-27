cbfdb87aab3518e35466553cc6e7f854080015df
S06 <735594423@qq.com>
2019-04-27 22:47:10 +0800

    修改规范

---
+++ model/coupon.go
@@ -0,0 +1,13 @@
+package model
+
+type CouponModel struct {
+	BaseModel
+}
+
+func (this *TypeModel) Queries() (interface{}, error) {
+
+}
+
+func (this *TypeModel) Template() (interface{}, error) {
+
+}
